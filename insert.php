887ecce1e002dbbfe6328d60e584d0f45b662b3a
SibgaShaikh <83008611+SibgaShaikh@users.noreply.github.com>
2022-06-09 15:33:56 +0530

    Update insert.php

--- insert.php
+++ insert.php
@@ -1,10 +1,10 @@
 <?php
-$con = mysql_connect("tcp:demosrv588.database.windows.net,1433","demoadmin588v","asdf@123");
+$con = mysql_connect("demosrv588.database.windows.net","demoadmin588v","asdf@123");
 if (!$con)
   {
   die('Could not connect: ' . mysql_error());
   }
-mysql_select_db("cis_id", $con);
+mysql_select_db("demodb588", $con);
 $sql="INSERT INTO nametable (fname, lname)
 VALUES
 ('$_POST[fname]','$_POST[lname]')";
